5377ae304fdd147600469d37d0f965e5a7a001ed
James Ruland <4818070+jmruland@users.noreply.github.com>
2025-04-11 10:54:55 -0400

    why

--- app/indexer.py
+++ app/indexer.py
@@ -1,5 +1,4 @@
-from llama_index.readers.file import SimpleDirectoryReader
-from llama_index import VectorStoreIndex
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 
 def load_index():
     reader = SimpleDirectoryReader(input_dir="./lore", recursive=True)
