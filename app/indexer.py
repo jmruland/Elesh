7630a9715182866c73513a2594b9dc2e9e978402
James Ruland <4818070+jmruland@users.noreply.github.com>
2025-04-11 12:43:45 -0400

    whoknows

--- app/indexer.py
+++ app/indexer.py
@@ -1,7 +1,12 @@
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, ServiceContext
+from llama_index.embeddings.ollama import OllamaEmbedding
 
 def load_index():
     reader = SimpleDirectoryReader(input_dir="./lore", recursive=True)
     docs = reader.load_data()
-    index = VectorStoreIndex.from_documents(docs)
+
+    embed_model = OllamaEmbedding(model_name="nomic-embed-text")
+    service_context = ServiceContext.from_defaults(embed_model=embed_model)
+
+    index = VectorStoreIndex.from_documents(docs, service_context=service_context)
     return index
