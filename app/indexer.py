be29085d4d51be5bf0a4aecbf9f90db56a6c741e
James Ruland <4818070+jmruland@users.noreply.github.com>
2025-04-11 13:11:31 -0400

    update after another fail

--- app/indexer.py
+++ app/indexer.py
@@ -1,4 +1,4 @@
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, ServiceContext
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
 from llama_index.embeddings.ollama import OllamaEmbedding
 
 def load_index():
@@ -6,7 +6,7 @@ def load_index():
     docs = reader.load_data()
 
     embed_model = OllamaEmbedding(model_name="nomic-embed-text")
-    service_context = ServiceContext.from_defaults(embed_model=embed_model)
+    Settings.embed_model = embed_model
 
-    index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+    index = VectorStoreIndex.from_documents(docs)
     return index
